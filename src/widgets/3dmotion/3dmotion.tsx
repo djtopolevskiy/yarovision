501540fbb6998d9d3c107294a13f26cbe4b5c845
djtopolevskiy <112426198+djtopolevskiy@users.noreply.github.com>
2024-04-21 20:41:47 +0300

    202404212042

--- src/widgets/3dmotion/3dmotion.tsx
+++ src/widgets/3dmotion/3dmotion.tsx
@@ -4,9 +4,10 @@ import Link from 'next/link'
 
 function Dmotion() {
   return (
-    <section className='w-full h-[400px] md:h-[600px] border-b border-custom flex justify-start items-center relative' >
-      <Image
-        src="/3DMotion.png"
+    <section className='w-full h-auto border-b border-custom flex justify-center items-center relative' >
+      <div className='flex relative w-[1730px] h-[400px] md:h-[600px] bg-gradient-to-r from-transparent to-gray-700 sm:pl-8 pt-12 sm:pt-32'>
+        <Image className=''
+        src="/3DMotion_1.png"
         // width={1730}
         // height={890}
         alt="Picture of the author"
@@ -16,17 +17,21 @@ function Dmotion() {
         //   width: '100%',
         //   height: 'auto',
         // }}
-        style={{ objectFit: "cover", objectPosition: "40%" }}
+        style={{ objectFit: "cover", objectPosition: "65%" }}
         priority={true}
         	fill={true}
-      />
-      <section className="flex flex-col z-10 sm:pl-8 pt-12 sm:pt-16 ">
-        <h1 className=" uppercase w-fit font-medium text-balance text-4xl md:text-5xl p-1">3D MOTION DESIGNER</h1>
-        <p className=" text-lg md:text-xl w-80 sm:w-auto text-balance leading-none font-normal p-1 ">A Worthy brand deserves worthy promotion.</p>
+        />
+
+        <section className="flex flex-col z-10 sm:pl-8 pt-12 sm:pt-18 ">
+        <h1 className="flex uppercase w-auto font-medium text-balance text-4xl md:text-5xl p-1">3D MOTION DESIGNER</h1>
+        <p className="flex text-lg md:text-xl w-auto sm:w-auto text-balance leading-none font-normal p-1 ">A Worthy brand deserves worthy promotion.</p>
         <div className='table p-1'>
           <Link className='mt-3 inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50' href="/#contactme">Contact</Link>
         </div>
       </section>
+      </div>
+      
+      
     </section>
   )
 }
--- src/widgets/header/_ui/layout.tsx
+++ src/widgets/header/_ui/layout.tsx
@@ -13,8 +13,8 @@ export default function Layout({
   burger?: React.ReactNode;
 }) {
   return (
-    <header className="flex items-center justify-between w-full text-[14px] text-opacity-60 fixed z-50">
-      <div className="sm:container flex h-11 items-center justify-between xl:1x2 w-full px-2 sm:px-10 backdrop-blur-xl my-1">
+    <header className="flex items-center justify-between w-full text-[14px] text-opacity-60 fixed z-50 backdrop-blur-xl my-1">
+      <div className="sm:container flex h-11 items-center justify-between xl:1x2 w-full px-2 sm:px-10 ">
         <div className="hover:text-primary flex w-[135px] h-11">{logo}</div>
         <div className='hidden md:flex'>
           {navtop}
